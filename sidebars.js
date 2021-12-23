cc555e407eb4027efa4475dd3b2db8ecbf4d3b91
xy2101 <77367387+xying21@users.noreply.github.com>
2021-12-23 11:34:59 +0800

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,5 +1,5 @@
 module.exports = {
 	sidebar2: [	{Introduction: ['overview','decentralizationRoadmap','comparisonEVM']},
-	'deployment','deployEthDapp','deposit_withdrawal','deposit_withdrawal_steps','faq'
+	'deployment','deployEthDapp','deposit_withdrawal','faq'
   ],
 };
